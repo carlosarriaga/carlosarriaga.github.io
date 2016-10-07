446ab3101714b8a28b414e4925d558d454470030
carlosarriaga <carlitos.arriaga@outlook.com>
2016-10-07 15:14:37 -0500

    new files
    new files

---
+++ carlos2/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+	$(function(){
+
+		$('.scrollspy').scrollSpy();
+
+		$('.parallax').parallax();
+
+		$('.tooltipped').tooltip({delay: 50});
+
+	}); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ carlos2/js/min/init-min.js
@@ -0,0 +1,1 @@
+!function($){$(function(){$(".scrollspy").scrollSpy(),$(".parallax").parallax(),$(".tooltipped").tooltip({delay:50})})}(jQuery);
