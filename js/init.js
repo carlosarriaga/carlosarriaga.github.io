a59f76aaf11b60c593556a17b4258f50dd41c428
carlosarriaga <carlitos.arriaga@outlook.com>
2016-10-15 22:39:00 -0500

    carga nuevo sitio
    carga nuevo sitio

--- js/init.js
+++ js/init.js
@@ -1,32 +1,11 @@
 (function($){
 	$(function(){
-		//$('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
 
-		//		$('.collapsible').collapsible({
-		//			accordion : false
-		//		});		
-		
 		$('.scrollspy').scrollSpy();
 
-		$('.parallax').parallax();		
-
-		$('.button-collapse').sideNav({
-			menuWidth: 400, // Default is 240
-			edge: 'left', // Choose the horizontal origin
-			closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		});
+		$('.parallax').parallax();
 
 		$('.tooltipped').tooltip({delay: 50});
-		
-		$('.modal-trigger').leanModal();
-		
-		
-	
-		
-
-
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
