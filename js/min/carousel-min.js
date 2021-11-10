3ff2e71f6ad9968e0c68885e4fe535dc046624bb
Carlos Arriaga <carlos.arriaga@live.com>
2021-11-10 17:53:49 -0600

    Update

---
+++ js/min/carousel-min.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){"use strict";$("#customers-testimonials").owlCarousel({loop:!0,center:!0,items:3,margin:0,autoplay:!0,dots:!0,autoplayTimeout:8500,smartSpeed:450,responsive:{0:{items:1},768:{items:2},1170:{items:3}}})});
